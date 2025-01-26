b9a145a1974c07753d3307e081ed8b9c5380f8b8
shfana <wambitafana@gmail.com>
2025-01-26 14:47:03 +0300

    Refactor: remove unnecessary error checks and remove the unnecessary variable declaration while clalling the text modification function

--- openfile/openfile.go
+++ openfile/openfile.go
@@ -12,23 +12,20 @@ import (
 // Function to open input file and create output file if one does not exist
 func OpenFile(inputFilepath string, outputFilepath string) error {
 	fileInfo, err := os.Stat(inputFilepath)
-	if os.IsNotExist(err) {
-		fmt.Println("File does not exist")
-		return err
-	}
 	if err != nil {
 		fmt.Printf("Error checking the file: %s\n", err)
 		return err
 	}
 	// check if the file size is greater than zero
-	if fileInfo.Size() < 0 {
+	if fileInfo.Size() == 0 {
 		fmt.Println("File exists but is empty")
 	}
-
+	//check file path  extension
 	if filepath.Ext(inputFilepath) != ".txt" {
 		fmt.Printf("Wrong file extension. Use .txt files")
 	}
 
+	//open the inout file
 	inputFile, err := os.Open(inputFilepath)
 	if err != nil {
 		fmt.Println(err)
@@ -44,13 +41,12 @@ func OpenFile(inputFilepath string, outputFilepath string) error {
 		fmt.Println(err)
 	}
 	defer outputFile.Close()
-
+	
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		line := scanner.Text()
-		line = goreloaded.TextModification(line)
-		fmt.Fprintln(outputFile, line)
-		}
+		fmt.Fprintln(outputFile, goreloaded.TextModification(line))
+	}
 
 	return nil
 }
